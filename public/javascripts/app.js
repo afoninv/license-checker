9fa6cf7290df1db64bd12e225f60fefff87829cb
Vitaly Afonin <afoninv@mail.ru>
2017-04-04 20:33:46 +0300

    Version 0.2.0, with file drop zone and getting classes data directly from codifiedsecurity/scan endpoint

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,5 @@
 'use strict';
 
-const config = {
-  loadExtraTimeout: 3000
-};
-
 class FilesArea extends React.Component {
   render() {
     return <textarea value={this.props.filesRaw} onChange={this.props.changeFiles}/>;
@@ -60,12 +56,14 @@ class App extends React.Component {
   constructor(props) {
     super(props);
 
+    this.onDrop = this.onDrop.bind(this);
     this.changeFiles = this.changeFiles.bind(this);
     this.toggleTabs = this.toggleTabs.bind(this);
     this.getLicensesFromServer = this.getLicensesFromServer.bind(this);
-    this.loadExtra = this.loadExtra.bind(this);
 
     this.state = {
+      apk: null,
+
       filesRaw: '',
       filesLength: 0,
       files: [],
@@ -98,15 +96,13 @@ class App extends React.Component {
   }
 
   toggleTabs(idx) {
-    return (event) => {
+    return () => {
       this.setState({ tab: idx });
     }
   }
 
   getLicensesFromServer() {
-    window.clearTimeout(this.state.loadingExtra);
-
-    this.setState({ spinner: true, loadingExtra: null }, () => {
+    this.setState({ spinner: true }, () => {
       window.fetch('/filenames/', {
         method: 'POST',
         headers: {
@@ -117,58 +113,57 @@ class App extends React.Component {
       }).then((res) => {
         return res.json();
       }).then((data) => {
-        let loadingExtra = null;
-
-        if (data.some((file) => {
-          return file.license.status === 'pending';
-        })) {
-          loadingExtra = window.setTimeout(this.loadExtra.bind(this), config.loadExtraTimeout);
-        };
-
-        this.setState({ spinner: false, serverResponse: data, loadingExtra });
+        this.setState({ spinner: false, serverResponse: data });
       });
     });
   }
 
-  loadExtra() {
-    let pendingFiles = this.state.serverResponse
-      .filter(function (file) {
-        return file.license.status === 'pending';
-      })
-      .map(function (file) {
-        return file.path;
+  onDrop(files) {
+    this.setState({
+      apk: files[0],
+      spinner: true
+    }, () => {
+      let formData = new FormData();
+      formData.append('file', this.state.apk, this.state.apk.name);
+
+      window.fetch('/apk-upload/', {
+        method: 'POST',
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then((res) => {
+        return res.json();
+      }).then((data) => {
+        //got data; extract license info
+        this.changeFiles({ target: { value: data.join('\n') } });
+        this.setState({ spinner: false, tab: 1 });
       });
-    if (!pendingFiles.length) {
-      return;
-    }
-
-    window.fetch('/filenames/', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(pendingFiles)
-    }).then((res) => {
-      return res.json();
-    }).then((data) => {
-      let loadingExtra = null;
-
-      if (data.some((file) => {
-        return file.license.status === 'pending';
-      })) {
-        loadingExtra = window.setTimeout(this.loadExtra.bind(this), config.loadExtraTimeout);
-      };
-
-      let newData = _.unionBy(data,  this.state.serverResponse, 'path');
-
-      this.setState({ serverResponse: newData, loadingExtra });
     });
   }
 
   render() {
 
-    let { tab, spinner, filesRaw, filesLength, files } = this.state;
+    let { tab, spinner, filesRaw, filesLength, files, apk } = this.state;
+
+    let tabRendered;
+    switch (tab) {
+      case 0:
+      default: // TODO base64 to file or css
+        tabRendered = (
+            <Dropzone className="drop-container" activeClassName="drop-container active" onDrop={this.onDrop} multiple={false}>
+              <h3>{ apk ? apk.name : 'Drop APK here'}</h3>
+              <img src="data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/PjxzdmcgaGVpZ2h0PSIxNnB4IiB2ZXJzaW9uPSIxLjEiIHZpZXdCb3g9IjAgMCAxNiAxNiIgd2lkdGg9IjE2cHgiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6c2tldGNoPSJodHRwOi8vd3d3LmJvaGVtaWFuY29kaW5nLmNvbS9za2V0Y2gvbnMiIHhtbG5zOnhsaW5rPSJodHRwOi8vd3d3LnczLm9yZy8xOTk5L3hsaW5rIj48dGl0bGUvPjxkZWZzLz48ZyBmaWxsPSJub25lIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiIGlkPSJJY29ucyB3aXRoIG51bWJlcnMiIHN0cm9rZT0ibm9uZSIgc3Ryb2tlLXdpZHRoPSIxIj48ZyBmaWxsPSIjMDAwMDAwIiBpZD0iR3JvdXAiIHRyYW5zZm9ybT0idHJhbnNsYXRlKC03NjguMDAwMDAwLCAtNDMyLjAwMDAwMCkiPjxwYXRoIGQ9Ik03NjksNDQ2IEw3ODEsNDQ2IEw3ODEsNDQzIEw3ODMsNDQzIEw3ODMsNDQ2IEw3ODMsNDQ4IEw3NjksNDQ4IFogTTc2OSw0NDMgTDc3MSw0NDMgTDc3MSw0NDYgTDc2OSw0NDYgWiBNNzc4LDQzOCBMNzc4LDQ0NSBMNzc0LDQ0NSBMNzc0LDQzOCBMNzcyLDQzOCBMNzc2LDQzMyBMNzgwLDQzOCBaIE03NzgsNDM4IiBpZD0iUmVjdGFuZ2xlIDIxNyBjb3B5Ii8+PC9nPjwvZz48L3N2Zz4=" />
+            </Dropzone>
+        );
+        break;
+      case 1:
+        tabRendered = ( <FilesArea filesRaw={filesRaw} changeFiles={this.changeFiles} /> );
+        break;
+      case 2:
+        tabRendered = ( <textarea readOnly={true} value={files.join('\n')} /> );
+        break;
+    }
 
     return (
       <div className={(spinner == true) ? 'app-container loading' : 'app-container'}>
@@ -176,21 +171,22 @@ class App extends React.Component {
 
         <ul className="nav nav-tabs">
           <li onClick={this.toggleTabs(0)} className={(tab === 0) ? 'active' : ''}>
-            <a href="#">Files ({filesLength} lines)</a>
+            <a href="#">APK Drop Area</a>
           </li>
           <li onClick={this.toggleTabs(1)} className={(tab === 1) ? 'active' : ''}>
+            <a href="#">Files ({filesLength} lines)</a>
+          </li>
+          <li onClick={this.toggleTabs(2)} className={(tab === 2) ? 'active' : ''}>
             <a href="#">Deduplicated ({files.length} lines)</a>
           </li>
         </ul>
 
-        {tab === 0 ? (
-          <FilesArea filesRaw={filesRaw} changeFiles={this.changeFiles} />      
-        ) : (
-          <textarea readOnly={true} value={files.join('\n')} />
-        )}
+        <div className="tab-container">
+          { tabRendered }
+        </div>
 
         <button className="btn btn-primary" disabled={files.length === 0} onClick={this.getLicensesFromServer}>
-          Send to server
+          Get license information
         </button>
 
         <Licenses data={this.state.serverResponse} />
---
+++ routes/apk-upload.js
@@ -0,0 +1,72 @@
+let express = require('express');
+let router = express.Router();
+
+let request = require('request');
+let requestP = require('request-promise');
+
+
+const config = {
+  apiKey: '552b9f47-e36a-4f8a-a1d4-cff9dc420332'
+};
+
+
+router.post('/', function(req, res, next) {
+  let scanConnection = request('https://api.codifiedsecurity.com/scan?apiKey=' + config.apiKey, function (err, resp, body) {
+      if (err) {
+        return res.json(err);
+      }
+
+      let { _id: id, status } = JSON.parse(body);
+
+      if (status !== 'completed') {
+        return res.json({ error: 'Unexpected response from scan' });
+      }
+  
+      let resultConnection = requestP(`https://api.codifiedsecurity.com/scans/${id}?apiKey=${config.apiKey}`)
+        .then(function (scanResultsRaw) {
+          let scanResults = JSON.parse(scanResultsRaw);
+          let memo = {};
+          findClasses(scanResults, memo);
+          let filesList = Object.keys(memo);
+          res.json(filesList);
+        })
+        .catch(function (err) {
+          res.json(err);
+        });
+
+  });
+
+  req.pipe(scanConnection);
+
+});
+
+function findClasses (obj, memo) {
+  if (!obj) {
+    return;
+  }
+
+  if (Array.isArray(obj)) {
+    for (let item of obj) {
+      findClasses(item, memo);
+    }
+  }
+
+  if (typeof(obj) === 'object') {
+
+    if (Array.isArray(obj.classes)) {
+      for (let cls of obj.classes) {
+        if (cls && typeof(cls) === 'object' && cls.class && typeof(cls.class) === 'string') {
+          memo[cls.class] = true;
+        }
+      }
+    }
+
+    for (let key in obj) {
+      if (key !== 'classes') {
+        findClasses(obj[key], memo);
+      }
+    }
+  }
+}
+
+module.exports = router;
