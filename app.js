604f558b4c33cd6dbb95a106b841a88e9b02dfd1
Vitaly Afonin <afoninv@mail.ru>
2017-04-07 17:28:20 +0300

    Dockerfile+test fix

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ var apkUpload = require('./routes/apk-upload');
 
 var app = express();
 
+//TESTS
+process.title = process.argv[2];
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
